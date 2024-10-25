5c292d03f3cb93d3619733d9c89d0dd0326c19bf
lopesrenato90 <blade.lopes@gmail.com>
2024-10-25 11:44:24 -0300

    Add files via upload

---
+++ OpenFile.java
@@ -0,0 +1,43 @@
+import javax.swing.*;
+import java.awt.*;
+import java.io.*;
+
+
+public class OpenFile {
+    public static void abrirArquivo() {
+        try {
+            JFileChooser chooser = new JFileChooser();
+
+            chooser.setDialogTitle("Selecione o arquivo que deseja abrir");
+            chooser.setApproveButtonText("Abrir arquivo");
+            int returnVal1 = chooser.showOpenDialog(null);
+            String fileFullPath = "";
+            if(returnVal1 == JFileChooser.APPROVE_OPTION) {
+                fileFullPath = chooser.getSelectedFile().getAbsolutePath();
+            } else {
+                System.out.println("Que pena!");
+            }
+        
+            try {
+                File file = new File(fileFullPath);
+                //Pode-se usar o operador ! (exclamação) para negar o resultado da expressão lógica (que esteja à sua direita)
+                if(!Desktop.isDesktopSupported()) {
+                    System.err.println("desktop is not supported in this machine");
+                    return;
+                }
+                Desktop desktop = Desktop.getDesktop();
+                if(file.exists()) {
+                    desktop.open(file);
+                }
+
+            }catch(Exception e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("Arquivo aberto com sucesso!"); 
+            } catch (Exception e) {
+                System.err.print("Não foi possível abrir o arquivo");
+            }
+        
+        }
+    }
--- RenameFile.java
+++ RenameFile.java
@@ -1,3 +1,8 @@
 public class RenameFile {
+
+    public static void renomearArquivo() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'renomearArquivo'");
+    }
     
 }
